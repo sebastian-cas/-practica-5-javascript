724283b35f2934a900224de4bee600d12efe8733
sebastian castillo <sebastiancastillo305@gmail.com>
2024-06-03 22:25:48 -0500

    agregar bucle for

--- script.js
+++ script.js
@@ -9,23 +9,23 @@ lineas
 // comenta una linea
 
 console.log("Hola Mundo")
-console.log("aqui podemos escribir cualquier cosa")
-console.log("adios")
-console.log("no hay autocompletado")
-console.log(12345)
-console.log(-1.123)
+// console.log("aqui podemos escribir cualquier cosa")
+// console.log("adios")
+// console.log("no hay autocompletado")
+// console.log(12345)
+// console.log(-1.123)
 
 // Tipos de datos
 
 /* declara una variable */
 let nombre = "Sebastian"
-console.log(nombre)
+// console.log(nombre)
 nombre = "Joan"
-console.log(nombre)
+// console.log(nombre)
 
 /* declara una constante */
 const pi = 3.1416
-console.log(pi)
+// console.log(pi)
 /* pi = 3  no se puede sobreescribir en una constante*/
 
 /* javascript antiguo */
@@ -34,7 +34,7 @@ var apellido = "castillo"
 const numero1 = 7
 const numero2 = 9
 const resultado = numero1 + numero2
-console.log("resultado:", resultado, "algo")
+// console.log("resultado:", resultado, "algo")
 
 // -------------- Tipos de datos ------------
 
@@ -42,7 +42,7 @@ console.log("resultado:", resultado, "algo")
 const ciudad = "bogota"
 const pais1 = 'colombia'
 const pais2 = "argentina"
-console.log(ciudad, pais1, pais2)
+// console.log(ciudad, pais1, pais2)
 
 // lueguaje debilmente Tipado
 // Int, float - numeros
@@ -50,8 +50,8 @@ const suma = 5
 const edad = 20.5
 const numeroNegativo = "-10"
 
-console.log("multiplicacion:", 5 * "5")
-console.log(suma, edad, numeroNegativo)
+// console.log("multiplicacion:", 5 * "5")
+// console.log(suma, edad, numeroNegativo)
 
 // Booleandos - Bool - verdaro o falso
 let esVerde = true
@@ -62,12 +62,12 @@ let esMorado = false
 // Array - Vectores - Listas
 
 let listaDeNumeros = [20, -55, 100]
-console.log(listaDeNumeros)
+// console.log(listaDeNumeros)
 
 // ver un elemento de la lista
-console.log(listaDeNumeros[0])
-console.log(listaDeNumeros[1])
-console.log(listaDeNumeros[2])
+// console.log(listaDeNumeros[0])
+// console.log(listaDeNumeros[1])
+// console.log(listaDeNumeros[2])
 
 let listaCiudades = [
     "bogotá",
@@ -75,16 +75,16 @@ let listaCiudades = [
     "cali"
 ]
 
-console.log(listaCiudades)
+// console.log(listaCiudades)
 
 listaCiudades[2] = "boyaca"
-console.log(listaCiudades)
+// console.log(listaCiudades)
 
 let listaDeListas = [listaDeNumeros, listaCiudades]
-console.log(listaDeListas)
+// console.log(listaDeListas)
 
 // acceder al dato "medellin"
-console.log(listaDeListas[1][1])
+// console.log(listaDeListas[1][1])
 
 // JSON (JavaScript Object Notation) - Objetos
 // clave - valor
@@ -99,9 +99,93 @@ let usuario = {
     amigos: ["andres", "pepito"]
 }
 
-console.log(usuario)
-console.log(usuario.nombre)
-console.log(usuario.ubicacion.latitud)
+// console.log(usuario)
+// console.log(usuario.nombre)
+// console.log(usuario.ubicacion.latitud)
 
 usuario.edad = 25
-console.log(usuario)
+// console.log(usuario)
+
+// --------- tipos de datos vacios ------
+
+// null - nulo
+let espacioVacio = null
+
+// undefined - indefinido
+let noDefinido = undefined
+// console.log(usuario.apellido)
+let algo
+// console.log(algo)
+
+// NaN - Not a Number - No es un numero
+const noEsNumero = NaN
+const multiplicacion = 10 * 'hola'
+// console.log(multiplicacion)
+
+
+// --------- Estructuras de control -------
+
+// Bucles - un proceso que se repite
+// loop - blucle infinito
+
+const listaAnimales = [
+    "perro",
+    "gato",
+    "oso",
+    "gallina",
+    "lobo",
+    "zorro"
+]
+
+console.log('tamaño del array:', listaAnimales.length)
+
+// parametro 1: contador
+// parametro 2: condicion para que se detenga
+// paramatro 3: comportamiento del contador
+// for(let contador = 0; contador < listaAnimales.length; contador = contador + 1) {
+for(let i = 0; i < listaAnimales.length; i) {
+    console.log(i)
+    console.log(listaAnimales[i])
+}
+
+// condiciones
+// - igualdad: ===
+// - menor que: <
+// - mayor que: >
+// - menor o igual que: <=
+// - mayor o igual que: >=
+
+
+
+const listaDeVideos = [
+    {
+        miniatura: "",
+        titulo: "",
+        canal: "",
+        visualizaciones: 0
+    },
+    {
+        miniatura: "",
+        titulo: "",
+        canal: "",
+        visualizaciones: 0
+    },
+    {
+        miniatura: "",
+        titulo: "",
+        canal: "",
+        visualizaciones: 0
+    },
+    {
+        miniatura: "",
+        titulo: "",
+        canal: "",
+        visualizaciones: 0
+    },
+    {
+        miniatura: "",
+        titulo: "",
+        canal: "",
+        visualizaciones: 0
+    }
+]
