9ee512cdbc56adcf7c595e0a6c82dd5ec2fc3575
sebastian castillo <sebastiancastillo305@gmail.com>
2024-06-03 21:28:06 -0500

    agregar declaracion de variable

---
+++ script.js
@@ -0,0 +1,37 @@
+/* agregar comentario: alt + shift + a */
+
+/* 
+comentar 
+varias 
+lineas 
+*/
+
+// comenta una linea
+
+console.log("Hola Mundo")
+console.log("aqui podemos escribir cualquier cosa")
+console.log("adios")
+console.log("no hay autocompletado")
+console.log(12345)
+console.log(-1.123)
+
+// Tipos de datos
+
+/* declara una variable */
+let nombre = "Sebastian"
+console.log(nombre)
+nombre = "Joan"
+console.log(nombre)
+
+/* declara una constante */
+const pi = 3.1416
+console.log(pi)
+/* pi = 3  no se puede sobreescribir en una constante*/
+
+/* javascript antiguo */
+var apellido = "castillo"
+
+const numero1 = 7
+const numero2 = 9
+const resultado = numero1 + numero2
+console.log("resultado:", resultado, "algo")
